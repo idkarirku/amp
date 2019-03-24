acec4caa01e89d6e0cda5bc85b9234672ef8da3f
idkarirku <idkarirku@gmail.com>
2019-03-24 19:36:29 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,3 +19,24 @@ self.addEventListener('install', function(event) {
 self.addEventListener('activate', function(event) {
   console.log('Service Worker activating.');  
 });
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/scripts/main.js',
+  '/images/smiley.svg'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Finally active. Ready to start serving content!');
+});
